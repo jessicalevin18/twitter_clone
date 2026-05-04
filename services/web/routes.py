9ebce412e8a78af342bb331ed2eaca51fbb4c4ff
Mike Izbicki <mike@izbicki.me>
2026-05-04 13:55:46 -0700

    [geni] Migrate HTML from routes to templates Move all of the inline HTML to files in the templates directory Create a base template and have all other templates extend it Install the Jinja2 templating engine and integrate with FastAPI routes

--- services/web/routes.py
+++ services/web/routes.py
@@ -1,171 +1,57 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi import Form
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 def read_root(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Home</title>
-      </head>
-      <body>
-          <h1>Welcome to the home page</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the home page"""
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @router.get("/login")
 def read_login(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Login</title>
-      </head>
-      <body>
-          <h1>Login page</h1>
-          <form action="/login" method="post">
-              <label for="username">Username:</label><br>
-              <input type="text" id="username" name="username" required><br>
-              <label for="password">Password:</label><br>
-              <input type="password" id="password" name="password" required><br>
-              <input type="submit" value="Submit">
-          </form>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the login page"""
+    return templates.TemplateResponse("login.html", {"request": request})
 
 @router.post("/login")
 def post_login(request: Request, username: str = Form(...), password: str = Form(...)):
-  html_content = """
-  <html>
-      <head>
-          <title>Login successful</title>
-      </head>
-      <body>
-          <h1>Login successful</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content after a successful login"""
+    return templates.TemplateResponse("login_successful.html", {"request": request})
 
 @router.get("/logout")
 def read_logout(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Logout</title>
-      </head>
-      <body>
-          <h1>You are logged out</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the logout page"""
+    return templates.TemplateResponse("logout.html", {"request": request})
 
 @router.get("/create_account")
 def read_create_account(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Create account</title>
-      </head>
-      <body>
-          <h1>Create account page</h1>
-          <form action="/create_account" method="post">
-              <label for="username">Username:</label><br>
-              <input type="text" id="username" name="username" required><br>
-              <label for="password">Password:</label><br>
-              <input type="password" id="password" name="password" required><br>
-              <label for="confirm_password">Confirm password:</label><br>
-              <input type="password" id="confirm_password" name="confirm_password" required><br>
-              <input type="submit" value="Submit">
-          </form>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the create account page"""
+    return templates.TemplateResponse("create_account.html", {"request": request})
 
 @router.post("/create_account")
 def post_create_account(request: Request, username: str = Form(...), password: str = Form(...), confirm_password: str = Form(...)):
-  html_content = """
-  <html>
-      <head>
-          <title>Account created</title>
-      </head>
-      <body>
-          <h1>Your account has been created</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content after a successful account creation"""
+    return templates.TemplateResponse("account_created.html", {"request": request})
 
 @router.get("/create_message")
 def read_create_message(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Create message</title>
-      </head>
-      <body>
-          <h1>Create message page</h1>
-          <form action="/create_message" method="post">
-              <label for="message">Message:</label><br>
-              <input type="text" id="message" name="message" required><br>
-              <input type="submit" value="Submit">
-          </form>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the create message page"""
+    return templates.TemplateResponse("create_message.html", {"request": request})
 
 @router.post("/create_message")
 def post_create_message(request: Request, message: str = Form(...)):
-  html_content = """
-  <html>
-      <head>
-          <title>Message posted</title>
-      </head>
-      <body>
-          <h1>Your message has been posted</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content after a successful message creation"""
+    return templates.TemplateResponse("message_posted.html", {"request": request})
 
 @router.get("/search")
 def read_search(request: Request):
-  html_content = """
-  <html>
-      <head>
-          <title>Search</title>
-      </head>
-      <body>
-          <h1>Search page</h1>
-          <form action="/search" method="post">
-              <label for="query">Query:</label><br>
-              <input type="text" id="query" name="query" required><br>
-              <input type="submit" value="Submit">
-          </form>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the search page"""
+    return templates.TemplateResponse("search.html", {"request": request})
 
 @router.post("/search")
 def post_search(request: Request, query: str = Form(...)):
-  html_content = """
-  <html>
-      <head>
-          <title>Search results</title>
-      </head>
-      <body>
-          <h1>Search results for your query</h1>
-      </body>
-  </html>
-  """
-  return HTMLResponse(content=html_content, status_code=200)
+    """Returns the HTML content for the search results page"""
+    return templates.TemplateResponse("search_results.html", {"request": request})
